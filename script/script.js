45b30cd004317951cae166b64a065c133620d0fc
DanielValeriote <danielvaleriote@outlook.com>
2022-03-03 14:28:49 -0300

    add 12 and 24 hours functionality

--- script/script.js
+++ script/script.js
@@ -1,7 +1,27 @@
+const day = new Date().getDay();
+setDay(day);
+
+let hourFormat = "24h";
+if (hourFormat == "24h") {
+  addClassActive("24h");
+  removeClassActive("12h");
+} else {
+  addClassActive("12h");
+  removeClassActive("24h");
+}
+
+function addClassActive(id) {
+  document.getElementById(id).classList.add("active");
+}
+
+function removeClassActive(id) {
+  document.getElementById(id).classList.remove("active");
+}
+
 const setDay = (d) => {
   switch (d) {
     case 0:
-      document.getElementById("sunday").classList.add("active")
+      document.getElementById("sunday").classList.add("active");
       break;
     case 1:
       document.getElementById("monday").classList.add("active");
@@ -13,35 +33,56 @@ const setDay = (d) => {
       document.getElementById("wednesday").classList.add("active");
       break;
     case 4:
-      document.getElementById("thursday").classList.add("active")
+      document.getElementById("thursday").classList.add("active");
       break;
     case 5:
-      document.getElementById("friday").classList.add("active")
+      document.getElementById("friday").classList.add("active");
       break;
     case 6:
-      document.getElementById("saturday").classList.add("active")
+      document.getElementById("saturday").classList.add("active");
       break;
     default:
-      alert("Error, day of the week could not be obtained")
+      alert("Error, day of the week could not be obtained");
   }
-}
+};
 
 const setClock = (h, m, s) => {
   const clock = document.getElementById("clock");
-  clock.innerText = `${h < 10 ? "0" + h : h}:${m < 10 ? "0" + m : m}:${s < 10 ? "0" + s : s}`;
-}
+  let treatedM = m < 10 ? "0" + m : m;
+  let treatedS = s < 10 ? "0" + s : s;
+
+  if (hourFormat === "24h") {
+    clock.innerText = `${h < 10 ? "0" + h : h}:${treatedM}:${
+      s < 10 ? "0" + s : s
+    }`;
+  } else {
+    let newHour = h;
+    if (h > 12) {
+      newHour = h - 12;
+      newHour = newHour < 10 ? "0" + newHour : newHour;
+    }
+    clock.innerText = `${newHour}:${treatedM}:${treatedS} PM`;
+  }
+};
 
 const updateClock = () => {
   const date = new Date();
   const hour = date.getHours();
   const min = date.getMinutes();
   const sec = date.getSeconds();
-
   setClock(hour, min, sec);
+};
+
+setInterval(updateClock, 1000);
+
+function setTwelve() {
+  hourFormat = "12h";
+  addClassActive("12h");
+  removeClassActive("24h");
 }
 
-const day = new Date().getDay();
-
-setDay(day)
-
-setInterval(updateClock, 1000)
+function setTwentyFour() {
+  hourFormat = "24h";
+  addClassActive("24h");
+  removeClassActive("12h");
+}
